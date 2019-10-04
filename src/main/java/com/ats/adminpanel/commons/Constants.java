8dece32dbd19656cf5d8efba830eb07a571c819f
ats-12 <ats-12@ats12>
2019-10-04 14:18:43 +0530

    //Mahesh

--- src/main/java/com/ats/adminpanel/commons/Constants.java
+++ src/main/java/com/ats/adminpanel/commons/Constants.java
@@ -3,8 +3,8 @@ package com.ats.adminpanel.commons;
 
 public class Constants {
 	
-	public static final String url="http://localhost:8097/";
-	//public static final String url="http://97.74.228.55:8080/webapi/";
+	//public static final String url="http://localhost:8097/";
+	public static final String url="http://97.74.228.55:8080/webapi/";
 	public static final String SPCAKE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
 	public static final String FR_IMAGE_URL = "http://97.74.228.55:8080/uploads/FR/";
 	public static final String MESSAGE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSG/";
@@ -30,8 +30,8 @@ public class Constants {
 	public static int RAW_MAT_IMAGE_TYPE=6;
     public static int GATE_ENTRY_IMAGE_TYPE=7;
 
-	//public static final String ReportURL ="http://localhost:8978/adminpanel/";
- 	public static final String ReportURL ="http://97.74.228.55:8080/admin/";//change
+    //public static final String ReportURL ="http://localhost:9090/adminpanel/";
+	public static final String ReportURL ="http://97.74.228.55:8080/admin/";//change
 	public static final String SETTING_KEY = "PB";
 	public static final int DIS_BY_ACC = 7;
 	public static final int AP_BY_ACC = 6;
@@ -40,7 +40,7 @@ public class Constants {
 	public static final int AP_BY_GATE = 2;
 	public static final int DIS_BY_GATE = 3;
 	public static final String REPORT_SAVE = "/opt/apache-tomcat-8.5.37/webapps/uploads/AdminReport.pdf";
-		public static final String BILL_REPORT_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/Bill.pdf";
+	public static final String BILL_REPORT_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/Bill.pdf";
 	public static final String CRN_REPORT_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/crn.pdf";
     public static final String FINISHEDGOOD_REPORT_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/fsr.pdf";
     public static final String PO_PDF_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/po.pdf";
@@ -48,12 +48,12 @@ public class Constants {
     public static final String INWARD_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/Inward.pdf";
     public static final String LOG_PDF_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/logdf.pdf";
     public static final String DISPATCH_PATH="/opt/apache-tomcat-8.5.37/webapps/uploads/dispatch.pdf";
-	//	public static final String REPORT_SAVE = "/home/ats-12/Report.pdf";
-
+		
 	public static final String SP_CAKE_FOLDER = "http://97.74.228.55:8080/uploads/SPCAKE/";
 	public static final String CUST_CHOICE_PHOTO_CAKE_FOLDER = "http://97.74.228.55:8080/uploads/CUSTCHOICEPHOTOCAKE/";
 	public static final int LOGIS_BILL_PDF_TYPE = 8;
 	public static final String LOGIS_BILL_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
 	public static final int MENU_IMAGE_TYPE = 0;
 	public static final String MENU_IMAGE_URL = null;
+	public static final String SPHSN = "19059010";
 }
